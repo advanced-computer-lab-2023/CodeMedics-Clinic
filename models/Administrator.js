87eb19866958fbbf73c50438baf1e9b0054756a7
Dido <didodada94@gmail.com>
2023-10-06 19:25:53 +0300

    - Updated the models to match the SRS

--- models/Administrator.js
+++ models/Administrator.js
@@ -4,25 +4,22 @@ const Schema = mongoose.Schema;
 const adminSchema = new Schema({
     Name: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a name'],
     },
     Username: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a username']
     },
     Password: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a password']
     },
     Email: {
         type: String,
-        required: true
+        required: [true, 'Please enter an email']
     },
-    Age: {
-        type: Number,
-        required: true,
-    }
-}, { timestamps: true });
+
+}, {timestamps: true});
 
 const Admin = mongoose.model('Admin', adminSchema);
 module.exports = Admin;
--- models/Doctor.js
+++ models/Doctor.js
@@ -2,27 +2,44 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const doctorSchema = new Schema({
-    Name: {
+    FirstName: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a first name']
+    },
+    LastName: {
+        type: String,
+        required: [true, 'Please enter a last name']
     },
     Username: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a username']
     },
     Password: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a password']
     },
     Email: {
         type: String,
-        required: true
+        required: [true, 'Please enter an email']
     },
-    Age: {
+    DateOfBirth: {
+        type: Date,
+        required: [true, 'Please enter a Date of Birth']
+    },
+    HourlyRate: {
         type: Number,
-        required: true,
+        required: [true, 'Please enter an hourly rate']
+    },
+    affiliation: {
+        type: String,
+        required: [true, 'Please enter an affiliation']
+    },
+    Degree: {
+        type: String,
+        required: [true, 'Please enter a degree']
+
     }
-}, { timestamps: true });
+}, {timestamps: true});
 
 const Doctor = mongoose.model('Doctor', doctorSchema);
 module.exports = Doctor;
