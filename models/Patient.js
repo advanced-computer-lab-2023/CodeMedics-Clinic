223ffbdea0e30694b55e21f4c1f8eba7eb3867a0
Dido <didodada94@gmail.com>
2023-10-06 19:26:12 +0300

    - Updated the models to match the SRS

--- models/Patient.js
+++ models/Patient.js
@@ -2,27 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const patientSchema = new Schema({
-    Name: {
+    FirstName: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a first name']
+    },
+    LastName: {
+        type: String,
+        required: [true, 'Please enter a last name']
     },
     Username: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a username']
     },
     Password: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a password']
     },
     Email: {
         type: String,
-        required: true
+        required: [true, 'Please enter an email']
     },
-    Age: {
+    DateOfBirth: {
+        type: Date,
+        required: [true, 'Please enter a Date of Birth']
+    },
+    Number: {
         type: Number,
-        required: true,
-    }
-}, { timestamps: true });
+        required: [true, 'Please enter a phone number']
+    },
+}, {timestamps: true});
 
 const Patient = mongoose.model('Patient', patientSchema);
 module.exports = Patient;
