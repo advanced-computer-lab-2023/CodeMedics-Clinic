a8fb8c6242fb17b39ff2b063317587ffb6165879
Dido <didodada94@gmail.com>
2023-10-11 20:23:56 +0300

    added routes for registeration feature

--- routes/PatientRoutes.js
+++ routes/PatientRoutes.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-
-const { addFamilyMember , viewFamilyMembers } = require('../controllers/Patient/FamilyMembersController');
-const {getPrescriptions, getPrescriptionsByDate, getPrescriptionsByDoctor, getPrescriptionsByStatus, filterPrescriptions} = require('../controllers/Patient/PrescriptionList');
+const patientController = require('../controllers/Patient/PatientController');
+const {addFamilyMember, viewFamilyMembers} = require('../controllers/Patient/FamilyMembersController');
+const {
+    getPrescriptions,
+    getPrescriptionsByDate,
+    getPrescriptionsByDoctor,
+    getPrescriptionsByStatus,
+    filterPrescriptions
+} = require('../controllers/Patient/PrescriptionList');
 const app = require('../app.js');
-const { filterAppointmentsPatient } = require('../controllers/Patient/filterAppointmentsPatient');
+const {filterAppointmentsPatient} = require('../controllers/Patient/filterAppointmentsPatient');
+
 function verifyToken(req, res, next) {
     const token = req.headers['token'];
-    try{
+    try {
         const model = jwt.verify(token, process.env.SECRET_KEY);
         res.locals.token = model;
         next();
-    }
-    catch(e){
+    } catch (e) {
         res.status(401).json({message: e.message});
     }
 }
 
+router.get('/register', patientController.viewPatientRegister);
+router.post('/register', patientController.createPatient);
 
 // app.use(verifyToken);
 
