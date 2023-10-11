1aeeb7d9171a806d615d6774018e821b78c10418
Yosef <65974533+koka-afk@users.noreply.github.com>
2023-10-12 00:18:25 +0300

    Search Logic fully finished

--- controllers/Patient/SearchDoctor.js
+++ controllers/Patient/SearchDoctor.js
@@ -4,16 +4,33 @@ const doctor = require('../../models/Doctor');
 
 
 exports.searchDoctor = async (req, res) => {
-    const doctorName = req.body.FirstName;// Assuming the request contains a 'FirstName' property.
-    console.log(doctorName);
+    const FirstName = req.query.FirstName;// Assuming the request contains a 'FirstName' property.
+    const LastName = req.query.LastName;
+    const Speciality = req.query.Speciality;
+    console.log(FirstName);
+    console.log(LastName);
+    console.log(Speciality);
+
+    const query = {};
+
+    if (FirstName.length > 0) {
+        query.FirstName = { $regex: FirstName, $options: 'i' };
+    }
+    if (LastName.length > 0) {
+        query.LastName = { $regex: LastName, $options: 'i' };
+    }
+    if (Speciality.length > 0) {
+        query.Specialty = { $regex: Speciality, $options: 'i' };
+    }
+    console.log(query);
+    if(Object.keys(query).length == 0){
+        res.status(400).json({ error: 'At least one search criterion must be provided.' });
+        return;
+    }
+
     // Query the Doctor model (assuming it's a Mongoose model) to find doctors by name.
     try {
-        const doctors = await doctor.find({
-            $or: [
-                { FirstName: { $regex: doctorName, $options: 'i' } },
-                { LastName: { $regex: doctorName, $options: 'i' } }
-            ]
-        });
+        const doctors = await doctor.find(query);
         res.status(200).json(doctors);
     } catch (err) {
         console.error(err);
