1a99f72ced7262cd96204965f0d9483f7e0890c0
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2023-10-14 23:08:20 +0300

    updated some methods

--- controllers/Doctor/filterPatients.js
+++ controllers/Doctor/filterPatients.js
@@ -3,10 +3,19 @@ const express = require('express');
 const Doctor = require('../../models/Doctor');
 const router = express.Router();
 const appointment = require('../../models/Appointment');
+const Patient = require('../../models/Patient');
 
 exports.filterPatients = async (req, res) => {
-    const username = req.query.username;
-    const Doctor = await Doctor.findOne({username: username});  
-    const patients = Doctor.Patients;
-    res.status(200).json({patients: patients});
+    try{
+    const {Username} = req.query;
+    const doctor = await Doctor.findOne({Username});  
+    const patients = [];
+    for(let i = 0; i < doctor.Patients.length; i++){
+        const patient = await Patient.findOne({_id: doctor.Patients[i]});
+        patients.push(patient);
+    }
+    res.status(200).json({data: patients});
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
 };;
