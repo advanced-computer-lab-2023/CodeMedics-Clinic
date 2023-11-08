251d5c6740b5145b22e87ccdcb6b43610d2a50dd
Nour Eldien Ayman <49641430+NourAlPha@users.noreply.github.com>
2023-11-08 20:01:44 +0200

    Added authentication

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['Patient', 'Doctor', 'Admin'],
+    default: 'Patient',
+    required: true,
+  },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
