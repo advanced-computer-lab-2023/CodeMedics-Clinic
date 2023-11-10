2528be35e31dc396ac0a08ac9afc2e0035852b16
Omar Osama <omarosama2114@gmail.com>
2023-11-10 18:33:11 +0300

    added el7a2ny-frontend

---
+++ Frontend/el7a2ny-frontend/src/pages/auth/doctorRegister.js
@@ -0,0 +1,271 @@
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Box, Button, Link, Stack, TextField, Typography } from '@mui/material';
+import { useAuth } from 'src/hooks/use-auth';
+import { Layout as AuthLayout } from 'src/layouts/auth/layout';
+import axios from 'axios';
+//import { error } from 'console';
+
+const Page = () => {
+  const router = useRouter();
+  const auth = useAuth();
+  const formik = useFormik({
+    initialValues: {
+      Name: '',
+      Username: '',
+      Password: '',
+      Email: '',
+      DateOfBirth: '',
+      HourlyRate: '',
+      affiliation: '',
+      Degree: '',
+        Submit: null
+    },
+    validationSchema: Yup.object({
+      Name: Yup
+        .string()
+        .max(255)
+        .required('Name is required'),
+      Username: Yup
+        .string()
+        .max(255)
+        .required('Username is required'),
+      Password: Yup
+        .string()
+        .max(255)
+        .required('Password is required'),
+      Email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      DateOfBirth: Yup
+        .date()
+        .required('Date of birth is required'),
+      HourlyRate: Yup
+        .number()
+        .required('Hourly rate is required'),
+      affiliation: Yup
+        .string()
+        .required('Affiliation is required'),
+      Degree: Yup
+        .string()
+        .required('Degree is required')  
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        const body = {"Name": values.Name, 
+        "Username":values.Username , 
+        "Password": values.Password,
+        "Email": values.Email,
+        "DateOfBirth": values.DateOfBirth,
+        "HourlyRate": values.HourlyRate,
+        "affiliation": values.affiliation,
+        "Degree": values.Degree };
+          await axios.post('http://localhost:8000/createPharmacist' , body)
+          .then((res) => { 
+            if(res.status != 200){
+              throw new Error(res.data.message); 
+            }
+              return res['data'];
+            })
+            .then((data) => {
+              router.push('/auth/login');
+            });
+      } catch (err) {
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ Submit: err.response.data.error});
+        helpers.setSubmitting(false);
+      }
+    }
+  });
+
+  return (
+    <>
+      <Head>
+        <title>
+          Pharmacist Register
+        </title>
+      </Head>
+      <Box
+        sx={{
+          flex: '1 1 auto',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            px: 3,
+            py: '100px',
+            width: '100%'
+          }}
+        >
+          <div>
+            <Stack
+              spacing={1}
+              sx={{ mb: 3 }}
+            >
+              <Typography variant="h4">
+                Register as Pharmacist
+              </Typography>
+              <Typography
+                color="text.secondary"
+                variant="body2"
+              >
+                Already have an account?
+                &nbsp;
+                <Link
+                  component={NextLink}
+                  href="/auth/login"
+                  underline="hover"
+                  variant="subtitle2"
+                >
+                  Log in
+                </Link>
+              </Typography>
+            </Stack>
+            <form
+              noValidate
+              onSubmit={formik.handleSubmit}
+            >
+              <Stack spacing={3}>
+                <TextField
+                  error={!!(formik.touched.Name && formik.errors.Name)}
+                  fullWidth
+                  helperText={formik.touched.Name && formik.errors.Name}
+                  label="Name"
+                  name="Name"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.Name}
+                />
+                <TextField
+                  error={!!(formik.touched.Username && formik.errors.Username)}
+                  fullWidth
+                  helperText={formik.touched.Username && formik.errors.Username}
+                  label="Username"
+                  name="Username"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.Username}
+                />
+                <TextField
+                  error={!!(formik.touched.Password && formik.errors.Password)}
+                  fullWidth
+                  helperText={formik.touched.Password && formik.errors.Password}
+                  label="Password"
+                  name="Password"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="password"
+                  value={formik.values.Password}
+                />
+                <TextField
+                  error={!!(formik.touched.Email && formik.errors.Email)}
+                  fullWidth
+                  helperText={formik.touched.Email && formik.errors.Email}
+                  label="Email Address"
+                  name="Email"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="email"
+                  value={formik.values.Email}
+                />
+                <TextField
+                  error={!!(formik.touched.DateOfBirth && formik.errors.DateOfBirth)}
+                  fullWidth
+                  helperText={formik.touched.DateOfBirth && formik.errors.DateOfBirth}
+                  label="Date of Birth"
+                  name="DateOfBirth"
+                  onBlur={formik.handleBlur}
+                  onChange={(event) => {
+                  const value = event.target.value;
+                  if (value.length <= 10) { // Limit the total length to 10 characters
+                    // Allow only digits (0-9) in the "yyyy" part
+                    const yyyy = value.slice(0, 4).replace(/[^0-9]/g, '');
+
+                    // Ensure "mm" and "dd" are not affected
+                    const mmdd = value.slice(4);
+
+                    // Combine the parts and format
+                    const formattedValue = `${yyyy}${mmdd}`;
+
+                    // Update the formik value
+                    formik.setFieldValue("DateOfBirth", formattedValue);
+                  }
+                }}
+                  type="date"
+                  value={formik.values.DateOfBirth}
+                  InputLabelProps={{ shrink: true }}
+                />
+                <TextField
+                  error={!!(formik.touched.HourlyRate && formik.errors.HourlyRate)}
+                  fullWidth
+                  helperText={formik.touched.HourlyRate && formik.errors.HourlyRate}
+                  label="Hourly Rate (EGP)"
+                  name="HourlyRate"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="number"
+                  value={formik.values.HourlyRate}
+                />
+                <TextField
+                  error={!!(formik.touched.affiliation && formik.errors.affiliation)}
+                  fullWidth
+                  helperText={formik.touched.affiliation && formik.errors.affiliation}
+                  label="Affiliation"
+                  name="affiliation"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.affiliation}
+                />
+                <TextField
+                  error={!!(formik.touched.Degree && formik.errors.Degree)}
+                  fullWidth
+                  helperText={formik.touched.Degree && formik.errors.Degree}
+                  label="Degree"
+                  name="Degree"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.Degree}
+                />
+              </Stack>
+              {formik.errors.Submit && (
+                <Typography
+                  color="error"
+                  sx={{ mt: 3 }}
+                  variant="body2"
+                >
+                  {formik.errors.Submit}
+                </Typography>
+              )}
+              <Button
+                fullWidth
+                size="large"
+                sx={{ mt: 3 }}
+                type="submit"
+                variant="contained"
+              >
+                Continue
+              </Button>
+            </form>
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => (
+  <AuthLayout>
+    {page}
+  </AuthLayout>
+);
+
+export default Page;
---
+++ Frontend/el7a2ny-frontend/src/sections/overview/overview-medicines.js
@@ -0,0 +1,179 @@
+import { useEffect, useState } from 'react';
+import { formatDistanceToNow } from 'date-fns';
+import PropTypes from 'prop-types';
+import ArrowRightIcon from '@heroicons/react/24/solid/ArrowRightIcon';
+import EllipsisVerticalIcon from '@heroicons/react/24/solid/EllipsisVerticalIcon';
+import Cookies from 'js-cookie';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  SvgIcon,
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Typography,
+} from '@mui/material';
+
+export const OverviewLatestProducts = (props) => {
+  const { products = [], sx } = props;
+
+  const [selectedQuantities, setSelectedQuantities] = useState({});
+
+  const username = Cookies.get("username");
+  console.log(username);
+  const handleQuantityChange = (event, productId) => {
+    const updatedQuantities = { ...selectedQuantities };
+    updatedQuantities[productId] = event.target.value;
+    setSelectedQuantities(updatedQuantities);
+  };
+
+ 
+  return (
+    <CardContent>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+        gap={2}
+      >
+        {products.map((product, index) => {
+
+            console.log(product);
+
+            const handleAddToCart = (productID) => {
+                const quantity = selectedQuantities[productID];
+                
+                console.log("in CART");
+                console.log(quantity);
+                console.log(productID);
+                if (quantity) {
+                  addToCartApiCall(username, productID, quantity);
+                }
+              };
+            
+              const addToCartApiCall = (username, productId, quantity) => {
+                fetch(`http://localhost:8000/updateMedicine`, {
+                  method: 'PATCH',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    Username: username,
+                    productID: productId,
+                    quantity,
+                  }),
+                })
+                  .then((response) => response.json())
+                  .then((data) => {
+                    console.log('Product added to cart:', data);
+                  })
+                  .catch((error) => {
+                    console.error('Error adding product to cart:', error);
+                  });
+              };
+            
+
+          return (
+            <Card
+              key={product._id}
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                height: '100%',
+                textAlign: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <ListItem
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  height: '100%',
+                  textAlign: 'center',
+                }}
+              >
+                <ListItemAvatar>
+                  {product.Picture ? (
+                    <Box
+                      component="img"
+                      src={product.Picture}
+                      sx={{
+                        borderRadius: '70%',
+                        height: 130,
+                        width: 130,
+                        objectFit: 'cover',
+                        margin: '0 auto',
+                      }}
+                    />
+                  ) : (
+                    <Box
+                      sx={{
+                        borderRadius: '50%',
+                        backgroundColor: 'neutral.200',
+                        height: 120,
+                        width: 120,
+                      }}
+                    />
+                  )}
+                </ListItemAvatar>
+                <ListItemText
+                  primary={product.name}
+                  primaryTypographyProps={{ variant: 'subtitle1' }}
+                  secondaryTypographyProps={{ variant: 'body2' }}
+                />
+              </ListItem>
+              <ListItemText
+                secondary={product.price}
+                primaryTypographyProps={{variant: 'subtitle2'}}
+                secondaryTypographyProps={{variant: 'body2'}}
+                />
+
+              <CardActions>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  size="small"
+                  onClick={handleAddToCart(product._id)}
+                >
+                  Add to Cart
+                </Button>
+                <Box sx={{ marginRight: 8 }} /> {/* Add space between the select and the button */}
+                <FormControl variant="outlined" size="small" sx={{ minWidth: 80 }}>
+                  <InputLabel id={`quantity-label-${product._id}`}>Quantity</InputLabel>
+                  <Select
+                    labelId={`quantity-label-${product._id}`}
+                    id={`quantity-${product._id}`}
+                    value={selectedQuantities[product._id] || 1}
+                    onChange={(event) => handleQuantityChange(event, product._id)}
+                  >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    {/* Add more options as needed */}
+                  </Select>
+                </FormControl>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </Box>
+      <Divider />
+    </CardContent>
+  );
+};
+
+OverviewLatestProducts.propTypes = {
+  products: PropTypes.array,
+  sx: PropTypes.object,
+};
