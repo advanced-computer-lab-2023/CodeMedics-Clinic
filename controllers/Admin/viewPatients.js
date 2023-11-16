a897621b999cf3c1c680101f83fa98f8842f4369
Omar Mohammad <89214824+OmarMUhammed03@users.noreply.github.com>
2023-11-16 14:10:11 +0200

    fixed a hot bug

--- controllers/Admin/viewPatients.js
+++ controllers/Admin/viewPatients.js
@@ -1,7 +1,8 @@
 
-const Patient = require('../../models/Doctor'); 
+const Patient = require('../../models/Patient'); 
 const viewPatients = async (req, res) => {
     const patients = await Patient.find();
+    console.log(patients);
     return res.status(200).json({patients});
 };
 
