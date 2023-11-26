1a19437312fa5a4d8f4e9d43240479bb74c6fc86
Nour Eldien Ayman <49641430+NourAlPha@users.noreply.github.com>
2023-11-26 21:31:56 +0200

    add phoneIcon in videocall

--- Frontend/el7a2ny-frontend/src/components/VideoCall.js
+++ Frontend/el7a2ny-frontend/src/components/VideoCall.js
@@ -1,6 +1,7 @@
 import Button from "@mui/material/Button"
 import IconButton from "@mui/material/IconButton"
 import TextField from "@mui/material/TextField"
+import SvgIcon from "@mui/material/SvgIcon"
 import ClipboardDocumentListIcon from "@heroicons/react/24/solid/ClipboardDocumentListIcon"
 import PhoneIcon from "@heroicons/react/24/solid/PhoneIcon"
 import React, { useEffect, useRef, useState } from "react"
@@ -10,26 +11,27 @@ import io from "socket.io-client"
 
 const socket = io.connect('http://localhost:8000')
 function VideoCall() {
-	const [ me, setMe ] = useState("")
-	const [ stream, setStream ] = useState()
-	const [ receivingCall, setReceivingCall ] = useState(false)
-	const [ caller, setCaller ] = useState("")
-	const [ callerSignal, setCallerSignal ] = useState()
-	const [ callAccepted, setCallAccepted ] = useState(false)
-	const [ idToCall, setIdToCall ] = useState("")
-	const [ callEnded, setCallEnded] = useState(false)
-	const [ name, setName ] = useState("")
+	const [me, setMe] = useState("")
+	const [stream, setStream] = useState()
+	const [receivingCall, setReceivingCall] = useState(false)
+	const [caller, setCaller] = useState("")
+	const [callerSignal, setCallerSignal] = useState()
+	const [callAccepted, setCallAccepted] = useState(false)
+	const [idToCall, setIdToCall] = useState("")
+	const [callEnded, setCallEnded] = useState(false)
+	const [name, setName] = useState("")
 	const myVideo = useRef()
 	const userVideo = useRef()
-	const connectionRef= useRef()
+	const connectionRef = useRef()
 
 	useEffect(() => {
 		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
 			setStream(stream)
+			if(myVideo.current)
 				myVideo.current.srcObject = stream
 		})
 
-	socket.on("me", (id) => {
+		socket.on("me", (id) => {
 			setMe(id)
 		})
 
@@ -56,9 +58,9 @@ function VideoCall() {
 			})
 		})
 		peer.on("stream", (stream) => {
-			
-				userVideo.current.srcObject = stream
-			
+
+			userVideo.current.srcObject = stream
+
 		})
 		socket.on("callAccepted", (signal) => {
 			setCallAccepted(true)
@@ -68,7 +70,7 @@ function VideoCall() {
 		connectionRef.current = peer
 	}
 
-	const answerCall =() =>  {
+	const answerCall = () => {
 		setCallAccepted(true)
 		const peer = new Peer({
 			initiator: false,
@@ -94,63 +96,72 @@ function VideoCall() {
 	return (
 		<>
 			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
-		<div className="container">
-			<div className="video-container">
-				<div className="video">
-					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
-				</div>
-				<div className="video">
-					{callAccepted && !callEnded ?
-					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
-					null}
-				</div>
-			</div>
-			<div className="myId">
-				<TextField
-					id="filled-basic"
-					label="Name"
-					variant="filled"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					style={{ marginBottom: "20px" }}
-				/>
-				<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-					<Button variant="contained" color="primary" startIcon={<ClipboardDocumentListIcon fontSize="large" />}>
-						Copy ID
-					</Button>
-				</CopyToClipboard>
-
-				<TextField
-					id="filled-basic"
-					label="ID to call"
-					variant="filled"
-					value={idToCall}
-					onChange={(e) => setIdToCall(e.target.value)}
-				/>
-				<div className="call-button">
-					{callAccepted && !callEnded ? (
-						<Button variant="contained" color="secondary" onClick={leaveCall}>
-							End Call
-						</Button>
-					) : (
-						<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-							<PhoneIcon fontSize="large" />
-						</IconButton>
-					)}
-					{idToCall}
-				</div>
-			</div>
-			<div>
-				{receivingCall && !callAccepted ? (
-						<div className="caller">
-						<h1 >{name} is calling...</h1>
-						<Button variant="contained" color="primary" onClick={answerCall}>
-							Answer
-						</Button>
+			<div className="container">
+				<div className="video-container">
+					<div className="video">
+						{stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
 					</div>
-				) : null}
+					<div className="video">
+						{callAccepted && !callEnded ?
+							<video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> :
+							null}
+					</div>
+				</div>
+				<div className="myId">
+					<TextField
+						id="filled-basic"
+						label="Name"
+						variant="filled"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						style={{ marginBottom: "20px" }}
+					/>
+					<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+						<Button variant="contained" color="primary" startIcon={<ClipboardDocumentListIcon fontSize="large" />}>
+							Copy ID
+						</Button>
+					</CopyToClipboard>
+
+					<TextField
+						id="filled-basic"
+						label="ID to call"
+						variant="filled"
+						value={idToCall}
+						onChange={(e) => setIdToCall(e.target.value)}
+					/>
+					<div className="call-button">
+						{callAccepted && !callEnded ? (
+							<Button variant="contained" color="secondary" onClick={leaveCall}>
+								End Call
+							</Button>
+						) : (
+							<IconButton
+							children={(
+							  <SvgIcon fontSize="small">
+								<PhoneIcon />
+							  </SvgIcon>
+							)}
+							color="primary"
+							onClick={() => {
+								callUser(idToCall);
+							}}
+						  >
+						  </IconButton >
+						)}
+						{idToCall}
+					</div>
+				</div>
+				<div>
+					{receivingCall && !callAccepted ? (
+						<div className="caller">
+							<h1 >{name} is calling...</h1>
+							<Button variant="contained" color="primary" onClick={answerCall}>
+								Answer
+							</Button>
+						</div>
+					) : null}
+				</div>
 			</div>
-		</div>
 		</>
 	)
 }
