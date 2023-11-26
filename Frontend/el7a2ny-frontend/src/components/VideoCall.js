fe61629faa003cbd36f87c54f05a5a6ca86a6770
Omar Mohammad <89214824+OmarMUhammed03@users.noreply.github.com>
2023-11-26 20:56:15 +0200

    fixed an import

--- Frontend/el7a2ny-frontend/src/components/VideoCall.js
+++ Frontend/el7a2ny-frontend/src/components/VideoCall.js
@@ -1,13 +1,12 @@
-import Button from "@material-ui/core/Button"
-import IconButton from "@material-ui/core/IconButton"
-import TextField from "@material-ui/core/TextField"
-import AssignmentIcon from "@material-ui/icons/Assignment"
-import PhoneIcon from "@material-ui/icons/Phone"
+import Button from "@mui/material/Button"
+import IconButton from "@mui/material/IconButton"
+import TextField from "@mui/material/TextField"
+import {AssignmentIcon} from "@mui/material"
+import {PhoneIcon} from "@mui/material"
 import React, { useEffect, useRef, useState } from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import Peer from "simple-peer"
 import io from "socket.io-client"
-import "./App.css"
 
 
 const socket = io.connect('http://localhost:8000')
