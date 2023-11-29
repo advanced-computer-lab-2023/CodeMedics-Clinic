00b20868ae161794da5f417d2458636e6c18e9d3
rawanelashmawy <rawanelashmawyy@gmail.com>
2023-11-29 02:43:02 +0200

    add/delete Med from Prescription

---
+++ models/Medicine.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const medicineSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter the medicine name'],
+    },
+    Description :{
+        type: String,
+        required: [true, 'Please enter the medicine description'],
+    },
+    activeIngredients: {
+        type: [String],
+        required: [true, 'Please enter the active ingredients'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please enter the price'],
+        min: [0, 'Price cannot be negative'],
+    },
+    medicalUse: {
+        type: String,
+        required: [true, 'Please enter the medical use'],
+    },
+    sales:{
+        type: Number,
+        default :0,
+    },
+    otc:{
+        type: Boolean,
+        default: true
+    },
+    availableQuantity: {
+        type: Number,
+        required: [true, 'Please enter the available quantity'],
+     
+    },Picture : {
+        type: String,
+        required: [true, 'Please enter the medicine picture']
+    },
+    
+}, { timestamps: true });
+
+
+const Medicine = mongoose.model('Medicine', medicineSchema);
+
+module.exports = Medicine;
--- routes/DoctorRoutes.js
+++ routes/DoctorRoutes.js
@@ -33,6 +33,8 @@ function verifyToken(req, res, next) {
 
 //const {addPrescription} = require('../controllers/Doctor/addPrescription');
 const {addPrescription} = require('../controllers/Doctor/addPrescription');
+const {addMedicineToPrescription , removeMedicineFromPrescription} = require('../controllers/Doctor/updatePrescriptionMed');
+
 
 router.post('/register', upload.fields([
     { name: 'nationalIdFile', maxCount: 1 },
@@ -97,6 +99,10 @@ router.get('/', (req, res) => {
 router.post('/:doctorUsername/addHealthRecord',uploadDocument, addHealthRecord);
 router.get('/:doctorUsername/health-records',docViewHealthRecords);
 router.post('/addPrescription', addPrescription);
+
+router.post('/addMedicineToPrescription', addMedicineToPrescription);
+router.post('/removeMedicineFromPrescription', removeMedicineFromPrescription);
+
 
 module.exports = router;
 
