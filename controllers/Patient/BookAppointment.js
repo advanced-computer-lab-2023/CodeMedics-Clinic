599bd40fb4a210eb1321ffb2dc8d20bfd026d629
mirnahaitham4 <mirnahaitha4@gmail.com>
2023-12-05 19:38:08 +0200

    book/cancel mail notifications

--- controllers/Patient/BookAppointment.js
+++ controllers/Patient/BookAppointment.js
@@ -1,29 +1,93 @@
-const express = require('express');
-const router = express.Router();
 const Doctor = require('../../models/Doctor');
 const Appointment = require('../../models/Appointment');
 const Patient = require('../../models/Patient');
+const nodemailer = require('nodemailer');
 
 exports.bookAppointment = async (req, res) => {
     try {
-        const { appointmentId, patientUsername } = req.query;
-        const appointment = await Appointment.findOne({_id: appointmentId});
+        const { appointmentId, patientUsername } = req.body;
+        const appointment = await Appointment.findOne({ _id: appointmentId }).populate('doctor');
+
+        if (!appointment) {
+            return res.status(400).json({ message: 'Appointment not found' });
+        }
+
+        if (appointment.status !== 'unreserved') {
+            return res.status(400).json({ message: 'Appointment cannot be booked. Status is not unreserved.' });
+        }
+
+        const patient = await Patient.findOne({ Username: patientUsername });
+        if (!patient) {
+            return res.status(400).json({ message: 'Patient not found' });
+        }
+
+        // Update appointment details
         appointment.patient = patientUsername;
-        appointment.status = "upcoming";
+        appointment.status = 'upcoming';
         await appointment.save();
-        const patient = await Patient.findOne({Username: patientUsername});
+
+        // Update patient's appointments
         patient.Appointments.push(appointmentId);
         await patient.save();
-        const doctor = await Doctor.findOne({Username: appointment.doctorUsername});
-        if(!doctor.Patients.includes(patient._id)){
+
+        // Update doctor's patients list
+        const doctor = await Doctor.findOne({ Username: appointment.doctorUsername });
+        if (!doctor) {
+            return res.status(400).json({ message: 'Doctor not found' });
+        }
+
+        if (!doctor.Patients.includes(patient._id)) {
             doctor.Patients.push(patient._id);
         }
+
         await doctor.save();
-        res.status(200).json({ message: "Appointment booked successfully" });
+
+        // Send email notification to patient
+        sendEmail(patient.Email, 'Appointment Confirmation', 'Your appointment has been booked successfully.');
+
+        // Send email notification to doctor
+        sendEmail(doctor.Email, 'New Appointment Booking', `Patient ${patient.FirstName} ${patient.LastName} has booked an appointment with you on ${appointment.date} from ${appointment.startHour} to ${appointment.endHour}.`);
+
+        // Response to the patient with appointment details
+        res.status(200).json({
+            message: 'Appointment booked successfully',
+            appointmentDetails: {
+                id: appointment._id,
+                doctorName: `${appointment.doctor.FirstName} ${appointment.doctor.LastName}`,
+                patientName: `${patient.FirstName} ${patient.LastName}`,
+                date: appointment.date,
+                startHour: appointment.startHour,
+                endHour: appointment.endHour,
+                status: appointment.status,
+            },
+        });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 };
 
+// Nodemailer setup and sendEmail function
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'mirnahaitham2@gmail.com',
+        pass: 'dygc irfq totb kuzy',
+    },
+});
 
+async function sendEmail(recipient, subject, message) {
+    try {
+        const mailOptions = {
+            from: 'mirnahaitham2@gmail.com', // Replace with your Gmail email
+            to: recipient,
+            subject: subject,
+            text: message,
+        };
 
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.response);
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw new Error('Failed to send email');
+    }
+}
--- controllers/Patient/CancelAppointment.js
+++ controllers/Patient/CancelAppointment.js
@@ -1,32 +1,86 @@
+
 const express = require('express');
 const router = express.Router();
-const Appointments = require('../../models/Appointment');
+const Appointment = require('../../models/Appointment');
 const Patient = require('../../models/Patient');
 const Doctor = require('../../models/Doctor');
+const nodemailer = require('nodemailer');
+
+
+async function sendEmail(recipient, subject, message) {
+    try {
+        const mailOptions = {
+            from: 'mirnahaitham2@gmail.com', // Replace with your Gmail email
+            to: recipient,
+            subject: subject,
+            text: message,
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.response);
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw new Error('Failed to send email');
+    }
+}
 
 exports.CancelAppointment = async (req, res) => {
-    try{
-        const { appointmentID } = req.body;
-        const appointment = await Appointments.findOne({_id: appointmentID});
+    try {
+        const { appointmentId } = req.body;
+
+        if (!appointmentId) {
+            return res.status(400).json({ success: false, message: 'Appointment ID is required' });
+        }
+
+        const appointment = await Appointment.findOne({ _id: appointmentId });
+
+        if (!appointment) {
+            return res.status(404).json({ success: false, message: 'Appointment not found' });
+        }
+
         const currentDate = new Date();
         const appointmentDate = new Date(appointment.date);
-        if(Math.abs(currentDate - appointmentDate) < 24){
+
+        if (Math.abs(currentDate - appointmentDate) < 24 * 60 * 60 * 1000) {
+            // Cancellation within 24 hours
             appointment.status = 'cancelled';
             await appointment.save();
-        }else{
-            const doctor = await Doctor.findOne({Username: appointment.doctorUsername});
-            const patient = await Patient.findOne({Username: appointment.patient});
+        } else {
+            // Cancellation beyond 24 hours
+            const doctor = await Doctor.findOne({ Username: appointment.doctorUsername });
+            const patient = await Patient.findOne({ Username: appointment.patient });
+            if (!doctor || !patient) {
+                return res.status(404).json({ success: false, message: 'Doctor or patient not found' });
+            }
+
             const appointmentPrice = Math.abs(parseInt(appointment.startHour) - parseInt(appointment.endHour));
-            doctor.Wallet = doctor.Wallet - appointmentPrice; // handle case that the wallet is initially empty ... or maybe it's a feature :)
-            patient.Wallet = patient.Wallet + appointmentPrice;
-            appointment.status = 'unreserved';
+            doctor.Wallet -= appointmentPrice;
+            patient.Wallet += appointmentPrice;
+
             await doctor.save();
             await patient.save();
+            appointment.status = 'unreserved';
             await appointment.save();
-        }
-        res.status(200).json({message: 'Appointment Cancelled Successfully'});
+        
 
-    }catch(error) {
-        return res.status(500).json({message: 'Something Wrong Happened while Cancelling'})
+        // Notify both doctor and patient
+        sendEmail(doctor.Email, 'Appointment Update', `Your appointment on ${appointment.date} has been canceled or rescheduled.`);
+        console.log("123456");
+
+        sendEmail(patient.Email, 'Appointment Update', `Your appointment on ${appointment.date} has been canceled or rescheduled.`);
+
+        res.status(200).json({ success: true, message: 'Appointment Canceled Successfully' });}
+    } catch (error) {
+        console.error('Error canceling appointment:', error);
+        return res.status(500).json({ success: false, message: 'Something went wrong while canceling the appointment' });
     }
 };
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'mirnahaitham2@gmail.com',
+        pass: 'dygc irfq totb kuzy',
+    },
+});
