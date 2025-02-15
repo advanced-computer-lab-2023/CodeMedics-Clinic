c0a65224c570e78ea220d890dd58a874de05cf5b
omar mohammed <omarb2515@gmail.com>
2025-02-15 23:36:14 +0200

    more reusable patient appointment info comp"

--- Frontend/el7a2ny-frontend/src/components/Appointment/PatientAppointmentInfo.js
+++ Frontend/el7a2ny-frontend/src/components/Appointment/PatientAppointmentInfo.js
@@ -1,13 +1,30 @@
 import { TableCell } from "@mui/material";
-function PatientAppointmentInfo({ appointment }) {
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  return (
-    <>
-      <TableCell>{new Date(appointment.date).toLocaleDateString()}</TableCell>
-      <TableCell>{days[new Date(appointment.date).getDay()]}</TableCell>
-      <TableCell>{appointment.startHour}</TableCell>
-      <TableCell>{appointment.endHour}</TableCell>
-    </>
-  );
+import { SeverityPill } from "../severity-pill";
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const statusMap = {
+  upcoming: "warning",
+  cancelled: "error",
+  completed: "success",
+  rescheduled: "warning",
+};
+
+function PatientAppointmentInfo({ appointment, attributes }) {
+  console.log("PAI", attributes);
+  const cells = attributes.map((item) => {
+    let content;
+    if (item === "date") {
+      content = new Date(appointment.date).toLocaleDateString();
+    } else if (item == "day") {
+      content = days[new Date(appointment.date).getDay()];
+    } else if (item === "status") {
+      content = (
+        <SeverityPill color={statusMap[appointment.status]}>{appointment.status}</SeverityPill>
+      );
+    } else {
+      content = appointment[item];
+    }
+    return <TableCell>{content}</TableCell>;
+  });
+  return cells;
 }
 export default PatientAppointmentInfo;
