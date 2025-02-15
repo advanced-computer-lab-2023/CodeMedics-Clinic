71ef5f23b886d6608f9561320f9c70e4d9a83894
omar mohammed <omarb2515@gmail.com>
2025-02-15 20:25:32 +0200

    made patient appointment comp more reusable

--- Frontend/el7a2ny-frontend/src/components/Appointment/PatientAppointment.js
+++ Frontend/el7a2ny-frontend/src/components/Appointment/PatientAppointment.js
@@ -1,33 +1,13 @@
-import {Stack, Typography} from "@mui/material";
-import { SeverityPill } from "../severity-pill";
-import PatientAppointmentActions from "./PatientAppointmentActions";
 import PatientAppointmentInfo from "./PatientAppointmentInfo";
 import Cell from "../Table/BasicElements/Cell";
 import Row from "../Table/BasicElements/Row";
 
-const statusMap = {
-  upcoming: "warning",
-  cancelled: "error",
-  completed: "success",
-  rescheduled: "warning",
-};
-
-function PatientAppointment({ appointment }) {
-  console.log("in the PA", appointment)
+function PatientAppointment({ appointment, attributes, actions }) {
+  console.log("PA", attributes)
   return (
-    <Row hover key={appointment._id}>
-      <Cell>
-        <Stack alignItems="center" direction="row" spacing={2}>
-          <Typography variant="subtitle2">{appointment.doctorUsername}</Typography>
-        </Stack>
-      </Cell>
-      <PatientAppointmentInfo appointment={appointment} />
-      <Cell>
-        <SeverityPill color={statusMap[appointment.status]}>{appointment.status}</SeverityPill>
-      </Cell>
-      <Cell>
-        <PatientAppointmentActions appointment={appointment} />
-      </Cell>
+    <Row hover key={appointment._id}> 
+      <PatientAppointmentInfo appointment={appointment} attributes={attributes} />
+      <Cell>{actions}</Cell>
     </Row>
   );
 }
