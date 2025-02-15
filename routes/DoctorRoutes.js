dcd535d461af5e934fc2e3c08ca9839fb75ec753
omar mohammed <omarb2515@gmail.com>
2025-02-15 21:09:53 +0200

    more RESTful apis

--- routes/DoctorRoutes.js
+++ routes/DoctorRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Doctor = require('../models/Doctor');
 const upload = require('../config/multerConfig'); 
-const jwt = require('jsonwebtoken');
 const { updateDoctor } = require('../controllers/Doctor/UpdateDoctor');
 const { filterAppointments , getAllApointments } = require('../controllers/Doctor/filterAppointments');
 const { viewUpcomingAppointments , viewPastAppointments, getAllDocAppointments } = require('../controllers/Doctor/viewAppointments');
@@ -10,12 +9,10 @@ const { searchPatient } = require('../controllers/Doctor/searchForPatient');
 const { viewPatients, getPatientByUsername } = require('../controllers/Doctor/viewPatients');
 const{addTimeSlot}=require('../controllers/Doctor/AvailableTImeSlots.js');
 const { filterPatients } = require('../controllers/Doctor/filterPatients');
-const fs = require('fs');
 const {createDoctor, viewDoctorRegister} = require('../controllers/Doctor/registerDoctor');
 const {getDoctors, getDoctorsAndAppointments} = require('../controllers/Doctor/GetDoctors');
 const {getAllDoctors} = require('../controllers/Doctor/registerDoctor');
 const { scheduleFollowUp } = require('../controllers/Doctor/ScheduleFollowup')
-const app = require('../app.js');
 const { requireAuth } = require('../Middleware/authMiddleware');
 const { changePassword } = require('../controllers/Doctor/registerDoctor');
 const {addAppointments} = require('../controllers/Doctor/addAppointment');
@@ -33,18 +30,6 @@ const {rescheduleAppointment} = require('../controllers/Doctor/rescheduleAppoint
 const {viewPatientAppointment} = require('../controllers/Doctor/viewPatientAppointment.js');
 const {getFollowRequests} = require('../controllers/Doctor/getFollowRequests');
 
-function verifyToken(req, res, next) {
-    const token = req.headers['token'];
-    try {
-        const model = jwt.verify(token, process.env.SECRET_KEY);
-        res.locals.token = model;
-        next();
-    } catch (e) {
-        res.status(401).json({message: e.message});
-    }
-}
-
-
 
 router.post('/checkMedicine',checkMedicine);
 
@@ -53,11 +38,21 @@ router.post('/register', upload.fields([
     { name: 'medicalDegreeFile', maxCount: 1 },
     { name: 'medicalLicenseFile', maxCount: 1 }
 ]), createDoctor);
+
 //app.use(verifyToken);
-router.get('/register', viewDoctorRegister);
+
+router.get('/patients', viewPatients);
+router.get('/:doctorUsername/patients/:patientUsername/appointments', viewPatientAppointment)
+router.get("/:doctorUsername/appointments", getAllDocAppointments);
+
+
+router.post('/:doctorUsername/appointments', addAppointments);
+
+
+
+
 router.get('/getAllDoctors', requireAuth, getAllDoctors);
 
-router.post('/addAppointments', addAppointments);
 
 
 router.post('/add-time-slot/:username', addTimeSlot);
@@ -77,46 +72,12 @@ router.patch('/rescheduleAppointments/:patientUsername', rescheduleAppointment);
 router.get('/viewAppointments', filterAppointments);
 router.get('/getAllAppointments', getAllApointments);
 router.get('/searchPatient', searchPatient);
-router.get('/viewPatients', viewPatients);
 router.get('/getDoctors', getDoctors);
 router.get('/filterPatients', filterPatients);
 router.get('/', getDoctorsAndAppointments);
-router.get("/getAllDocAppointments", getAllDocAppointments);
 router.get('/getPatientByUsername', getPatientByUsername);
 
-router.get('/viewPatientDetails:Username', (req, res) => {
-    const Username = req.params.Username;
-    console.log(Username);
-    res.setHeader('Content-Type', 'text/html');
-    const path = './views/p/viewPatientDetails';
-    path += '/' + Username;
-    path += '.html';
-    fs.readFile(path, null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Whoops! File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
 
-});
-router.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    const username = req.query.Username;
-    console.log("in the doctor route");
-    console.log(username);
-    fs.readFile('./views/Doctor/Doctor.html', null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Whoops! File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
-});
 router.post('/:doctorUsername/addHealthRecord',uploadDocument, addHealthRecord);
 router.get('/:doctorUsername/health-records',docViewHealthRecords);
 router.post('/addPrescription', addPrescription);
