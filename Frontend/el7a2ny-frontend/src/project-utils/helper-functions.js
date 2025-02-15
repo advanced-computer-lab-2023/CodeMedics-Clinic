6fd30273a1a1e38888ca6173bea99a9aa01416bf
omar mohammed <omarb2515@gmail.com>
2025-02-15 20:24:39 +0200

    add helper functions and custom hooks

---
+++ Frontend/el7a2ny-frontend/src/project-utils/helper-functions.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+function sortByDate(data) {
+  data.sort((a, b) => {
+    if (new Date(a.Date) < new Date(b.Date)) return -1;
+    if (new Date(a.Date) > new Date(b.Date)) return 1;
+    return 0;
+  });
+  return data;
+}
+
+function fixFormDate(date) {
+  if (date.length <= 10) {
+    const yyyy = date.slice(0, 4).replace(/[^0-9]/g, "");
+    const mmdd = date.slice(4);
+    const formattedDate = `${yyyy}${mmdd}`;
+    return formattedDate;
+  }
+}
+
+async function GET({ url, setData, setLoading, setShowError, setError }) {
+  await axios
+    .get(url)
+    .then((response) => {
+      if (setData) setData(response.data.data);
+      if (setLoading) setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setShowError(true);
+      setError(error.response.data.message);
+    });
+}
+
+async function POST({ url, body, updater, setLoading, setShowError, setError }) {
+  await axios
+    .post(url, body)
+    .then(() => {
+      if (updater) updater();
+      if (setLoading) setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setShowError(true);
+      setError(error.response.data.message);
+    });
+}
+
+async function PATCH({ url, body, updater, setLoading, setShowError, setError }) {
+  await axios
+    .patch(url, body)
+    .then(() => {
+      if (updater) updater();
+      if (setLoading) setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setShowError(true);
+      setError(error.response.data.message);
+    });
+}
+
+async function DELETE({ url, body, updater, setLoading, setShowError, setError }) {
+  await axios
+    .delete(url, body)
+    .then(() => {
+      if (updater) updater();
+      if (setLoading) setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setShowError(true);
+      setError(error.response.data.message);
+    });
+}
+
+export { sortByDate, fixFormDate, POST, PATCH, DELETE, GET };
