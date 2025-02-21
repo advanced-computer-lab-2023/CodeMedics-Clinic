15260aea34377e4ac5954daacfa5865925c264be
omar mohammed <omarb2515@gmail.com>
2025-02-21 14:04:50 +0200

    refactor chat messages comp

--- Frontend/el7a2ny-frontend/src/sections/user/chat/ChatMessages.js
+++ Frontend/el7a2ny-frontend/src/sections/user/chat/ChatMessages.js
@@ -1,30 +1,34 @@
-import { Stack, Box, Container, Divider, Unstable_Grid2 as Grid, Typography, Avatar, Card, OutlinedInput, InputAdornment, SvgIcon, IconButton, Tooltip } from '@mui/material';
-import { Message } from './Message';
-import {useEffect, useRef} from 'react';
-
+import {
+  Stack,
+} from "@mui/material";
+import { Message } from "./Message";
+import { useEffect, useRef } from "react";
+import Cookies from "js-cookie";
 
 export const ChatMessages = (props) => {
-    const { messages, username } = props;
-    const ref = useRef(null);
+  const { messages } = props;
+  const ref = useRef(null);
+  const username = Cookies.get("username");
 
-    useEffect(() => {
-        if(ref.current) {
-            ref.current.scrollIntoView({behavior: 'smooth', block: 'end'});
-        }
-    }, [messages]);
-    return (
-        <div>
-        <Stack spacing={2}
-        sx={{ p: 3 }}>
-            {messages.map((message, index) => {
-                return (
-                    <Message message={message.content} position={message.sender == username ? 'right' : 'left'} />
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  }, [messages]);
 
-                )
-            })
-            }
-        </Stack>
-        <div ref={ref}/>
-        </div>
-    );
+  return (
+    <div>
+      <Stack spacing={2} sx={{ p: 3 }}>
+        {messages.map((message, index) => {
+          return (
+            <Message
+              message={message.content}
+              position={message.sender == username ? "right" : "left"}
+            />
+          );
+        })}
+      </Stack>
+      <div ref={ref} />
+    </div>
+  );
 };
