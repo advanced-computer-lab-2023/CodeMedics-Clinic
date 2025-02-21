6df28ada3ac6a73d7995e3635aa18bf8e24d7c45
omar mohammed <omarb2515@gmail.com>
2025-02-21 22:22:10 +0200

    moved chat comps from sections to components

---
+++ Frontend/el7a2ny-frontend/src/components/Chat/Chat.js
@@ -0,0 +1,167 @@
+import Head from "next/head";
+import { Box, Divider } from "@mui/material";
+import { ChatSidebar } from "./ChatSidebar";
+import { ChatBox } from "./ChatBox";
+import socket from "src/components/socket";
+import { useEffect, useState } from "react";
+import { Stack } from "@mui/system";
+import Cookies from "js-cookie";
+import Message from "src/components/Miscellaneous/Message";
+import { BACKEND_ROUTE } from "src/project-utils/constants";
+import { useGet } from "src/hooks/custom-hooks";
+import LoadingSpinner from "src/components/Miscellaneous/LoadingSpinner";
+import { GET, POST } from "src/project-utils/helper-functions";
+import NoChat from "src/components/Miscellaneous/NoChat";
+
+const Chat = ({isPatient}) => {
+  const username = Cookies.get("username");
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [messagesCache, setMessagesCache] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [showError, setShowError] = useState(false);
+  const [error, setError] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const route = isPatient ? "patients" : "doctors"
+
+  useGet({
+    url: `${BACKEND_ROUTE}/${route}/${username}/chats`,
+    setData: setChats,
+    setLoading,
+    setShowError,
+    setError,
+  });
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      if (selectedChat) {
+        await getMessages(selectedChat.chat._id);
+        if (messagesCache[selectedChat.chat._id]) setMessages(messagesCache[selectedChat.chat._id]);
+      }
+    };
+
+    fetchMessages();
+  }, [selectedChat, messagesCache]);
+
+  const updateChats = (message) => {
+    const updatedChats = chats
+      .map((chat) => {
+        if (chat.chat._id == message.chat) {
+          return {
+            ...chat,
+            chat: { latestMessage: message, updatedAt: message.createdAt },
+            latestMessage: message,
+          };
+        }
+        return chat;
+      })
+      .sort((a, b) => new Date(b.chat.updatedAt) - new Date(a.chat.updatedAt));
+    setChats(updatedChats);
+    if (messagesCache[message.chat]) {
+      setMessagesCache((prev) => ({
+        ...prev,
+        [message.chat]: [...(prev[message.chat] || []), message],
+      }));
+    } else {
+      GET({
+        url: `${BACKEND_ROUTE}/${route}/chats/${message.chat}/messages`,
+        setData: (data) => {
+          setMessagesCache((prev) => ({ ...prev, [message.chat]: data }));
+        },
+        setShowError,
+        setError,
+      });
+    }
+  };
+
+  socket.off("newMessage").on("newMessage", (message) => {
+    updateChats(message);
+  });
+
+  socket.off("newMessagePharmacy").on("newMessagePharmacy", (message) => {
+    updateChats(message);
+  });
+
+  const getMessages = async (chatId) => {
+    if (chatId && !messagesCache[chatId]) {
+      GET({
+        url: `${BACKEND_ROUTE}/${route}/chats/${chatId}/messages`,
+        setData: (data) => {
+          setMessagesCache((prev) => ({ ...prev, [chatId]: data }));
+        },
+        setShowError,
+        setError,
+      });
+    }
+  };
+
+  const sendMessage = (message) => {
+    const body = {
+      sender: username,
+      content: message,
+    };
+
+    console.log("message", message);
+
+    POST({
+      url: `${BACKEND_ROUTE}/${route}/chats/${selectedChat.chat._id}/messages`,
+      body,
+      setShowError,
+      setError,
+      updater: () => {
+        const newMessage = {
+          chat: selectedChat.chat._id,
+          sender: body.sender,
+          content: body.content,
+        };
+        updateChats(newMessage);
+        const eventName = selectedChat.pharmacy ? "newMessagePharmacy" : "newMessage";
+        const payload = selectedChat.pharmacy
+          ? { message: newMessage, receiver: Cookies.get("username"), sendingToPharmacy: true }
+          : { message: newMessage, receiver: isPatient ? selectedChat.doctor.username : selectedChat.patient.username };
+
+        socket.emit(eventName, payload);
+      },
+    });
+  };
+
+  console.log("chat", selectedChat, messages, messagesCache);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>El7a2ny Clinic</title>
+      </Head>
+      <Message
+        condition={showError}
+        setCondition={setShowError}
+        title={"Error"}
+        message={error}
+        buttonAction={"Close"}
+      />
+      <Box>
+        <Divider />
+        <Stack direction="row">
+          <ChatSidebar
+            chats={chats}
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+          />
+          <Divider orientation="vertical" flexItem />
+          {selectedChat == null ? (
+            <NoChat />
+          ) : (
+            <ChatBox selectedChat={selectedChat} messages={messages} sendMessage={sendMessage} />
+          )}
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default Chat;
