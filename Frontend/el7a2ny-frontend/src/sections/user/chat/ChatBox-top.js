0a90d59378768a8e21b3ba0209be9f5f2089cac0
omar mohammed <omarb2515@gmail.com>
2025-02-21 14:05:38 +0200

    refactor chat box-top comp

--- Frontend/el7a2ny-frontend/src/sections/user/chat/ChatBox-top.js
+++ Frontend/el7a2ny-frontend/src/sections/user/chat/ChatBox-top.js
@@ -1,33 +1,53 @@
-import { Stack, Box, Container, Divider, Unstable_Grid2 as Grid, Typography, Avatar, Card, OutlinedInput, InputAdornment, SvgIcon, IconButton, Tooltip } from '@mui/material';
-import CameraIcon from '@heroicons/react/24/solid/CameraIcon';
-import { useRouter } from 'next/navigation';
+import {
+  Stack,
+  Typography,
+  Avatar,
+  SvgIcon,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+import CameraIcon from "@heroicons/react/24/solid/CameraIcon";
+import { useRouter } from "next/navigation";
 
 export const ChatBoxTop = (props) => {
-    const router = useRouter();
-    const { selectedChat } = props;
-    return(
-        <Stack sx={{ m: 2 }} direction="row" justifyContent="space-between">
-                <Stack direction="row" >
-                    <Avatar alt={selectedChat.doctor.FirstName + " " + selectedChat.doctor.LastName} src={selectedChat.doctor.Picture == null ? `/assets/avatars/0.png` : selectedChat.doctor.Picture} />
-                    <Stack sx={{ ml: 2, mt: 0.5 }} >
-                        <Typography variant='body1' >
-                            {selectedChat.doctor.FirstName + " " + selectedChat.doctor.LastName}
-                        </Typography>
-                        <Typography variant='caption' color="textSecondary">
-                            {selectedChat.doctor.Speciality}
-                        </Typography>
-                    </Stack>
-                </Stack>
-                <Tooltip title="Video Call">
-                    <IconButton onClick={() => {router.push(`/user/videoCall?username=${selectedChat.doctor.Username}`)}}>
-                        <SvgIcon
-                            color="action"
-                            fontSize="small"
-                        >
-                            <CameraIcon />
-                        </SvgIcon>
-                    </IconButton>
-                </Tooltip>
-            </Stack>
-    );
+  const router = useRouter();
+  const { selectedChat, isPharmacy } = props;
+  const name = isPharmacy
+    ? "Code Medics Pharmacy"
+    : `${selectedChat.doctor.firstName} ${selectedChat.doctor.lastName}`;
+
+  const src = isPharmacy
+    ? `/assets/Pharmacy-Logo.png`
+    : selectedChat.doctor.picture == null
+    ? `/assets/avatars/0.png`
+    : selectedChat.doctor.picture;
+
+  return (
+    <Stack sx={{ m: 2 }} direction="row" justifyContent="space-between">
+      <Stack direction="row">
+        <Avatar alt={name} src={src} />
+        <Stack sx={{ ml: 2, mt: 0.5 }}>
+          <Typography variant="body1">{name}</Typography>
+          {!isPharmacy && (
+            <Typography variant="caption" color="textSecondary">
+              {selectedChat.doctor.speciality}
+            </Typography>
+          )}
+        </Stack>
+      </Stack>
+      {!isPharmacy && (
+        <Tooltip title="Video Call">
+          <IconButton
+            onClick={() => {
+              router.push(`/patient/videoCall?username=${selectedChat.doctor.username}`);
+            }}
+          >
+            <SvgIcon color="action" fontSize="small">
+              <CameraIcon />
+            </SvgIcon>
+          </IconButton>
+        </Tooltip>
+      )}
+    </Stack>
+  );
 };
