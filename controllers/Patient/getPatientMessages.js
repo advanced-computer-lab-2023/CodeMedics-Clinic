96f1b3819e8373e3f6a4f0e1b64207a8f75470bb
omar mohammed <omarb2515@gmail.com>
2025-02-25 14:04:06 +0200

    simplify get patient message code

--- controllers/Patient/getPatientMessages.js
+++ controllers/Patient/getPatientMessages.js
@@ -1,18 +1,13 @@
-const Patient = require('../../models/Patient'); // Import the Patient model
+const Patient = require("../../models/Patient"); // Import the Patient model
+const { validatePatient } = require("../../utils/validator");
 
 exports.getPatientMessages = async (req, res) => {
   try {
-    const {patientUsername} = req.params;
-    const patient = await Patient.findOne({ username: patientUsername });
-
-    if (!patient) {
-      console.log('Patient not found.');
-      return res.status(404).json({ message: 'Patient not found' });
-    }
-
-    res.status(200).json({ data: patient.messages });
+    const { patientUsername } = req.params;
+    const patient = await validatePatient(patientUsername, res);
+    res.status(200).json({ data: patient.messages.reverse() });
   } catch (error) {
-    console.error('Error retrieving patient messages:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error retrieving patient messages:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
