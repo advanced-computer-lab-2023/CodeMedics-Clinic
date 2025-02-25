ef6a3537e003451e7c15499efe328c10d2247b94
omar mohammed <omarb2515@gmail.com>
2025-02-25 13:51:17 +0200

    add video-chat for doctor & reorder tabs

--- Frontend/el7a2ny-frontend/src/layouts/dashboard/doctor/config.js
+++ Frontend/el7a2ny-frontend/src/layouts/dashboard/doctor/config.js
@@ -1,83 +1,83 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import CalendarDaysIcon from '@heroicons/react/24/solid/CalendarDaysIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
-import { SvgIcon } from '@mui/material';
-import ChatBubbleLeftRightIcon from '@heroicons/react/24/solid/ChatBubbleLeftRightIcon';
-import MessageChatSquareIcon from '../../../icons/untitled-ui/duocolor/message-chat-square';
-import CreditCard01Icon from '../../../icons/untitled-ui/duocolor/credit-card-01';
-import HomeSmileIcon from '../../../icons/untitled-ui/duocolor/home-smile';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import SettingsIcon from '@mui/icons-material/Settings';
+import CalendarDaysIcon from "@heroicons/react/24/solid/CalendarDaysIcon";
+import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
+import VideoCameraIcon from "@heroicons/react/24/solid/VideoCameraIcon";
+import { SvgIcon } from "@mui/material";
+import MessageChatSquareIcon from "../../../icons/untitled-ui/duocolor/message-chat-square";
+import HomeSmileIcon from "../../../icons/untitled-ui/duocolor/home-smile";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 export const items = [
   {
-    title: 'Patients',
-    path: '/doctor/patients',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UsersIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Appointments',
-    path: '/doctor/appointments',
-    icon: (
-      <SvgIcon fontSize="small">
-        <CalendarDaysIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Follow-Up Requests',
-    path: '/doctor/follow-up-requests',
-    icon: (
-      <SvgIcon fontSize="small">
-        <CalendarDaysIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Prescriptions',
-    path: '/doctor/AllPrescriptions',
-    icon: (
-      <SvgIcon fontSize="small">
-        <AssignmentIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Chat',
-    path: '/doctor/chat',
-    icon: (
-      <SvgIcon fontSize="small">
-        <MessageChatSquareIcon   />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Account',
-    path: '/doctor/account',
+    title: "Account",
+    path: "/doctor/account",
     icon: (
       <SvgIcon fontSize="small">
         <HomeSmileIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Settings',
-    path: '/doctor/settings',
+    title: "Patients",
+    path: "/doctor/patients",
     icon: (
       <SvgIcon fontSize="small">
-        <SettingsIcon  />
+        <UsersIcon />
       </SvgIcon>
-    )
+    ),
   },
-  
+  {
+    title: "Appointments",
+    path: "/doctor/appointments",
+    icon: (
+      <SvgIcon fontSize="small">
+        <CalendarDaysIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Prescriptions",
+    path: "/doctor/AllPrescriptions",
+    icon: (
+      <SvgIcon fontSize="small">
+        <AssignmentIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Follow-Up Requests",
+    path: "/doctor/follow-up-requests",
+    icon: (
+      <SvgIcon fontSize="small">
+        <CalendarDaysIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Chat",
+    path: "/doctor/chat",
+    icon: (
+      <SvgIcon fontSize="small">
+        <MessageChatSquareIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "CodeMedics Meet",
+    path: "/rooms/video-chat",
+    icon: (
+      <SvgIcon fontSize="small">
+        <VideoCameraIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Settings",
+    path: "/doctor/settings",
+    icon: (
+      <SvgIcon fontSize="small">
+        <SettingsIcon />
+      </SvgIcon>
+    ),
+  },
 ];
