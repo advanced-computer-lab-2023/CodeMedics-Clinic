ea80fe86122d7cda3353dd90da2db919bb2d6c4b
omar mohammed <omarb2515@gmail.com>
2025-03-14 01:13:34 +0200

    refactor patient appointment actions

--- Frontend/el7a2ny-frontend/src/components/Appointment/PatientAppointmentActions.js
+++ Frontend/el7a2ny-frontend/src/components/Appointment/PatientAppointmentActions.js
@@ -1,72 +1,17 @@
-import { useContext, useEffect, useState } from "react";
-import { Table, TableBody } from "@mui/material";
+import { useContext, useState } from "react";
 import ChecklistIcon from "src/icons/untitled-ui/duocolor/ChecklistIcon";
 import RescheduleIcon from "src/icons/untitled-ui/duocolor/RescheduleIcon";
 import CancelIcon from "src/icons/untitled-ui/duocolor/CancelIcon";
 import { TableContext } from "../Table/Table";
-import LoadingSpinner from "../LoadingSpinner";
-import PopUp from "../Miscellaneous/PopUp";
-import Head from "../Table/Body/Head";
-import ButtonElement from "../ButtonElement";
-import Cell from "../Table/BasicElements/Cell";
-import Row from "../Table/BasicElements/Row";
 import Icon from "../Icon";
 import { BACKEND_ROUTE } from "src/project-utils/constants";
-import { useGet } from "src/hooks/custom-hooks";
 import { PATCH } from "src/project-utils/helper-functions";
-import ObjectInfo from "../ObjectInfo";
-const attributes = ["date", "day", "startHour", "endHour"];
+import ReschedulePopUp from "../ReschedulePopUp";
 
 function PatientAppointmentActions({ appointment }) {
-  const { setShowError, setError, setAllData, setPopUpDisplay, setPopUpElement } =
-    useContext(TableContext);
+  const { setShowError, setError, setAllData } = useContext(TableContext);
   const [loading, setLoading] = useState(true);
   const [rescheduling, setRescheduling] = useState(false);
-  const [unreservedAppointments, setUnreservedAppointments] = useState([]);
-  const unreservedAppointmentsElements = unreservedAppointments.map((item) => {
-    return (
-      <Row key={item._id}>
-        <ObjectInfo obj={item} attributes={attributes} />
-        <Cell>
-          <ButtonElement
-            actionName="Reschedule"
-            onClick={() => {
-              hanldeRescheduleAppointment(item, appointment);
-              setRescheduling(false);
-              setPopUpDisplay(false);
-            }}
-          />
-        </Cell>
-      </Row>
-    );
-  });
-
-  const reschedulePopUp = (
-    <PopUp
-      title={"Reschedule Appointment"}
-      viewing={rescheduling}
-      setViewing={setRescheduling}
-      actionName="Close"
-      setPopUpDisplay={setPopUpDisplay}
-    >
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <Table>
-          {unreservedAppointments.length ? (
-            <>
-              <Head columns={["Date", "Day", "From", "To"]} />
-              <TableBody>{unreservedAppointmentsElements}</TableBody>
-            </>
-          ) : (
-            <Row>
-              <Cell>Sorry, No Available Slots!</Cell>
-            </Row>
-          )}
-        </Table>
-      )}
-    </PopUp>
-  );
 
   async function handleRequestFollowUp(appointmentId) {
     PATCH({
@@ -82,35 +27,6 @@ function PatientAppointmentActions({ appointment }) {
           })
         );
       },
-    });
-  }
-
-  async function hanldeRescheduleAppointment(newApp, oldApp) {
-    PATCH({
-      url: `${BACKEND_ROUTE}/patients/appointments/${newApp._id}`,
-      body: { status: "rescheduled", patientUsername: oldApp.patientUsername },
-      setShowError,
-      setError,
-      updater: () => {
-        setAllData((prev) =>
-          prev.map((item) => {
-            if (item._id !== oldApp._id) return item;
-            return {
-              ...item,
-              _id: newApp._id,
-              status: "rescheduled",
-              patientUsername: oldApp.patientUsername,
-            };
-          })
-        );
-      },
-    });
-
-    PATCH({
-      url: `${BACKEND_ROUTE}/patients/appointments/${oldApp._id}`,
-      body: { status: "unresrved", patientUsername: null },
-      setShowError,
-      setError,
     });
   }
 
@@ -130,22 +46,6 @@ function PatientAppointmentActions({ appointment }) {
       },
     });
   }
-
-  useEffect(() => {
-    if (rescheduling) {
-      setPopUpDisplay(true);
-      setPopUpElement(reschedulePopUp);
-    }
-  }, [rescheduling, loading]);
-
-  useGet({
-    url: `${BACKEND_ROUTE}/patients/doctors/${appointment.doctorUsername}/appointments?status=unreserved`,
-    setData: setUnreservedAppointments,
-    setLoading: setLoading,
-    setShowError,
-    setError,
-    dependency: [appointment.doctorUsername],
-  });
 
   return (
     <>
@@ -175,6 +75,15 @@ function PatientAppointmentActions({ appointment }) {
       >
         <CancelIcon />
       </Icon>
+      <ReschedulePopUp
+        appointment={appointment}
+        getUrl={`${BACKEND_ROUTE}/patients/doctors/${appointment.doctorUsername}/appointments`}
+        patchUrl={`${BACKEND_ROUTE}/patients/appointments`}
+        loading={loading}
+        setLoading={setLoading}
+        rescheduling={rescheduling}
+        setRescheduling={setRescheduling}
+      />
     </>
   );
 }
