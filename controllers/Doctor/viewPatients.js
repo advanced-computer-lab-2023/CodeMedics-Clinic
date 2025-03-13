d010bdfccd63f17044d7b5620dbf4080191eac4e
omar mohammed <omarb2515@gmail.com>
2025-03-13 22:07:58 +0200

    update view patients

--- controllers/Doctor/viewPatients.js
+++ controllers/Doctor/viewPatients.js
@@ -2,22 +2,19 @@ const Doctor = require("../../models/Doctor");
 const Patient = require("../../models/Patient");
 const Appointment = require("../../models/Appointment");
 const { getUsername } = require("../../config/infoGetter");
+const { validateDoctor } = require("../../utils/validator");
 
 exports.viewPatients = async (req, res) => {
   try {
-    const username = await getUsername(req, res);
-
-    if (!username)
-      return res.status(401).json({ message: "You are not logged in." });
-    const doctor = await Doctor.findOne({ username: username });
-    if (!doctor) return res.status(401).json({ message: "Doctor not found." });
+    const { doctorUsername } = req.params;
+    const doctor = await validateDoctor(doctorUsername, res);
     const patients = [];
     if (doctor.patients == null) return res.status(200).json({ patients });
     for (var i = 0; i < doctor.patients.length; i++) {
       const patient = await Patient.findOne({ _id: doctor.patients[i] });
       if (patient) {
         const appointment = await Appointment.findOne({
-          patient: patient.username,
+          patientUsername: patient.username,
           status: "upcoming",
         });
         if (appointment) {
