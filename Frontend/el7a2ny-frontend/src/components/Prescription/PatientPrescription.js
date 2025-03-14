bf4a978ed8bf302c2534fcc4d117a1d891d6ae68
omar mohammed <omarb2515@gmail.com>
2025-03-14 03:59:50 +0200

    update patient prescription

--- Frontend/el7a2ny-frontend/src/components/Prescription/PatientPrescription.js
+++ Frontend/el7a2ny-frontend/src/components/Prescription/PatientPrescription.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { TableRow, TableCell, Stack, Typography, IconButton, SvgIcon } from "@mui/material";
 import { SeverityPill } from "src/components/severity-pill";
 import PatientPrescriptionActions from "./PatientPrescriptionActions";
+import ObjectInfo from "../ObjectInfo";
 
 const statusMap = {
   filled: "success",
@@ -9,25 +10,16 @@ const statusMap = {
 };
 
 function PatientPrescription({ prescription }) {
-
   const [state, setState] = useState(prescription);
   const status = state.filled ? "filled" : "unfilled";
 
   useEffect(() => {
     setState(prescription);
   }, [prescription]);
-  
+
   return (
     <TableRow hover key={state._id}>
-      <TableCell>
-        <Stack alignItems="center" direction="row" spacing={2}>
-          <Typography variant="subtitle2">{state.Doctor}</Typography>
-        </Stack>
-      </TableCell>
-      <TableCell>{state.Date}</TableCell>
-      <TableCell>
-        <SeverityPill color={statusMap[status]}>{status}</SeverityPill>
-      </TableCell>
+      <ObjectInfo obj={state} attributes={["doctorUsername", "date", "status"]} />
       <PatientPrescriptionActions setState={setState} state={state} />
     </TableRow>
   );
