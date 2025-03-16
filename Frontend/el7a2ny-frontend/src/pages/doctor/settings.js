16bcbff051127bcc54bdfd5dafe3aeac7b678f74
omar mohammed <omarb2515@gmail.com>
2025-03-16 02:30:47 +0200

    refactor doctor settings page

--- Frontend/el7a2ny-frontend/src/pages/doctor/settings.js
+++ Frontend/el7a2ny-frontend/src/pages/doctor/settings.js
@@ -1,29 +1,23 @@
-import Head from 'next/head';
-import { Box, Container, Stack, Typography } from '@mui/material';
-import { SettingsNotifications } from 'src/sections/settings/settings-notifications';
-import { SettingsPassword } from 'src/sections/doctor/settings/settings-password';
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/doctor/layout';
+import Head from "next/head";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/doctor/layout";
+import { SettingsPassword } from "src/components/SettingsPassword";
 
 const Page = () => (
   <>
     <Head>
-      <title>
-        Settings
-      </title>
+      <title>Settings</title>
     </Head>
     <Box
       component="main"
       sx={{
         flexGrow: 1,
-        py: 8
+        py: 8,
       }}
     >
       <Container maxWidth="lg">
         <Stack spacing={3}>
-          <Typography variant="h4">
-            Settings
-          </Typography>
-          {/* <SettingsNotifications /> */}
+          <Typography variant="h4">Settings</Typography>
           <SettingsPassword />
         </Stack>
       </Container>
@@ -31,10 +25,6 @@ const Page = () => (
   </>
 );
 
-Page.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Page;
