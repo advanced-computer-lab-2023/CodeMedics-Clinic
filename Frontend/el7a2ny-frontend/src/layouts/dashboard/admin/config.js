2d7e631a33e5af581c5e595a3cc8d6f684531917
omar mohammed <omarb2515@gmail.com>
2025-03-16 09:29:49 +0200

    update admin config

--- Frontend/el7a2ny-frontend/src/layouts/dashboard/admin/config.js
+++ Frontend/el7a2ny-frontend/src/layouts/dashboard/admin/config.js
@@ -1,11 +1,6 @@
-import ChartBarIcon from "@heroicons/react/24/solid/ChartBarIcon";
 import CogIcon from "@heroicons/react/24/solid/CogIcon";
-import LockClosedIcon from "@heroicons/react/24/solid/LockClosedIcon";
-import ShoppingBagIcon from "@heroicons/react/24/solid/ShoppingBagIcon";
 import UserIcon from "@heroicons/react/24/solid/UserIcon";
-import UserPlusIcon from "@heroicons/react/24/solid/UserPlusIcon";
 import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
-import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
 import { SvgIcon } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import ChecklistIcon from "@mui/icons-material/Checklist";
