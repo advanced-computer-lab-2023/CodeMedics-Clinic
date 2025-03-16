d39919b9e74df1fbad0c1cc206dcacc9a9796a4a
omar mohammed <omarb2515@gmail.com>
2025-03-16 06:29:17 +0200

    update account details to be reusable

--- Frontend/el7a2ny-frontend/src/components/Account/AccountDetails.js
+++ Frontend/el7a2ny-frontend/src/components/Account/AccountDetails.js
@@ -1,122 +1,56 @@
 import { useState } from "react";
-import axios from "axios";
-import { BACKEND_ROUTE, patientUpdateRoute } from "src/project-utils/constants";
+import { BACKEND_ROUTE } from "src/project-utils/constants";
 import Form from "../Form";
 import { PATCH } from "src/project-utils/helper-functions";
+import Cookies from "js-cookie";
 
-function AccountDetails({ user }) {
-  const values = {
-    firstName: user.firstName,
-    lastName: user.lastName,
-    username: user.username,
-    email: user.email,
-    number: user.number,
-    dateOfBirth: user.dateOfBirth,
-    emergencyContactName: user.emergencyContact.name,
-    emergencyContactNumber: user.emergencyContact.number,
-    emergencyContactRelation: user.emergencyContact.relation,
+function AccountDetails({ user, fields, setShowError, setError }) {
+  const createInitialValues = () => {
+    const initialValues = {};
+    fields.forEach((field) => {
+      if (field.name.includes(".")) {
+        const [parent, child] = field.name.split(".");
+        initialValues[field.name] = user[parent]?.[child] || "";
+      } else {
+        initialValues[field.name] = user[field.name] || "";
+      }
+    });
+    return initialValues;
   };
 
-  const [firstName, setFirstName] = useState(user.firstName);
-  const [lastName, setLastName] = useState(user.lastName);
-  const [username, setUsername] = useState(user.username);
-  const [email, setEmail] = useState(user.email);
-  const [number, setNumber] = useState(user.number);
-  const [dateOfBirth, setDateOfBirth] = useState(user.dateOfBirth);
-  const [emergencyContactName, setEmergencyContactName] = useState(user.emergencyContact.name);
-  const [emergencyContactNumber, setEmergencyContactNumber] = useState(
-    user.emergencyContact.number
-  );
-  const [emergencyContactRelation, setEmergencyContactRelation] = useState(
-    user.emergencyContact.relation
-  );
-  const [showError, setShowError] = useState(false);
-  const [error, setError] = useState("");
+  const [formValues, setFormValues] = useState(createInitialValues());
 
-  const fields = [
-    {
-      name: "firstName",
-      label: "First Name",
-      type: "text",
-      value: firstName,
-      setValue: setFirstName,
+  const formFields = fields.map((field) => ({
+    ...field,
+    value: formValues[field.name],
+    setValue: (value) => {
+      setFormValues((prev) => ({
+        ...prev,
+        [field.name]: value,
+      }));
     },
-    {
-      name: "lastName",
-      label: "Last Name",
-      type: "text",
-      value: lastName,
-      setValue: setLastName,
-    },
-    {
-      name: "username",
-      label: "Username",
-      type: "text",
-      value: username,
-      setValue: setUsername,
-    },
-    {
-      name: "email",
-      label: "Email",
-      type: "email",
-      value: email,
-      setValue: setEmail,
-    },
-    {
-      name: "number",
-      label: "Phone Number",
-      type: "text",
-      value: number,
-      setValue: setNumber,
-    },
-    {
-      name: "dateOfBirth",
-      label: "Date Of Birth",
-      type: "date",
-      value: dateOfBirth,
-      setValue: setDateOfBirth,
-    },
-    {
-      name: "emergencyContactName",
-      label: "Emergency Contact Name",
-      type: "text",
-      value: emergencyContactName,
-      setValue: setEmergencyContactName,
-    },
-    {
-      name: "emergencyContactNumber",
-      label: "Emergency Contact Number",
-      type: "text",
-      value: emergencyContactNumber,
-      setValue: setEmergencyContactNumber,
-    },
-    {
-      name: "emergencyContactRelation",
-      label: "Emergency Contact Relation",
-      type: "text",
-      value: emergencyContactRelation,
-      setValue: setEmergencyContactRelation,
-    },
-  ];
+  }));
 
-  const onSubmit = async (user, helpers) => {
+  const onSubmit = async (submittedValues, helpers) => {
     try {
-      const body = {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        username: user.username,
-        email: user.email,
-        dateOfBirth: user.dateOfBirth,
-        number: user.number,
-        gender: user.gender,
-        emergencyContact: {
-          name: user.emergencyContactName,
-          number: user.emergencyContactNumber,
-          relation: user.emergencyContactRelation,
-        },
-      };
+      const body = {};
+
+      Object.keys(submittedValues).forEach((key) => {
+        if (key.includes(".")) {
+          const [parent, child] = key.split(".");
+          if (!body[parent]) body[parent] = {};
+          body[parent][child] = submittedValues[key];
+        } else {
+          body[key] = submittedValues[key];
+        }
+      });
+
+      const apiEndpoint = `${BACKEND_ROUTE}/${Cookies.get("isDoctor") ? "doctors" : "patients"}/${
+        user.username
+      }`;
+
       PATCH({
-        url: `${BACKEND_ROUTE}/patients/${username}`,
+        url: apiEndpoint,
         body,
         setShowError,
         setError,
@@ -134,7 +68,13 @@ function AccountDetails({ user }) {
 
   return (
     <div>
-      <Form title="Profile" fields={fields} actionName="save" onSubmit={onSubmit} values={values} />
+      <Form
+        title="Profile"
+        fields={formFields}
+        actionName="save"
+        onSubmit={onSubmit}
+        values={formValues}
+      />
     </div>
   );
 }
