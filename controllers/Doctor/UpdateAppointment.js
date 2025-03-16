a3e6bb282200926846c4f5192fc1a1967b1bbe56
omar mohammed <omarb2515@gmail.com>
2025-03-16 02:17:53 +0200

    add update appointment

--- controllers/Doctor/UpdateAppointment.js
+++ controllers/Doctor/UpdateAppointment.js
@@ -18,6 +18,25 @@ exports.CompleteAppointment = async (req, res) => {
   }
 };
 
+exports.UpdateAppointment = async (req, res) => {
+  try {
+    const { appointmentId } = req.params;
+    const { status, patientUsername } = req.body;
+    console.log("completing", appointmentId, req.body);
+    const appointment = await validateAppointment(appointmentId, res);
+    console.log(appointment);
+    appointment.status = status;
+    appointment.patientUsername = patientUsername;
+    await appointment.save();
+    res.status(201).json({ message: "Appointment Updated Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Something Wrong Happened while Cancelling" });
+  }
+};
+
 exports.DeleteAppointment = async (req, res) => {
   try {
     const { appointmentId } = req.params;
