df4c79c8759ba4171b7c86264eeb998b25d61ead
omar mohammed <omarb2515@gmail.com>
2025-03-17 06:52:47 +0200

    update admin controller

--- controllers/Admin/removeDoctor.js
+++ controllers/Admin/removeDoctor.js
@@ -1,10 +1,8 @@
-
-const Doctor = require('../../models/Doctor'); 
+const Doctor = require("../../models/Doctor");
 const removeDoctor = async (req, res) => {
-    const { Username } = req.body;
-    const doctor = await Doctor.findOne({Username: Username });
-    await Doctor.deleteOne({Username: Username });
-    return res.status(200).json({message: 'Doctor removed.'});
+  const { doctorUsername } = req.params;
+  await Doctor.deleteOne({ username: doctorUsername });
+  return res.status(200).json({ message: "Doctor removed." });
 };
 
 module.exports = removeDoctor;
--- controllers/Admin/viewDoctors.js
+++ controllers/Admin/viewDoctors.js
@@ -1,8 +1,7 @@
-
-const Doctor = require('../../models/Doctor'); 
+const Doctor = require("../../models/Doctor");
 const viewDoctors = async (req, res) => {
-    const doctors = await Doctor.find({Status: "Approved"});
-    return res.status(200).json({doctors});
+  const doctors = await Doctor.find({ status: "approved" });
+  return res.status(200).json({ data: doctors });
 };
 
 module.exports = viewDoctors;
