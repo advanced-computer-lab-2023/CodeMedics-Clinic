c5b0e40abcda2d14346b9158cc95b59771040501
omar mohammed <omarb2515@gmail.com>
2025-03-20 06:50:49 +0200

    remove unused code

--- Frontend/el7a2ny-frontend/src/layouts/auth/layout.js
+++ Frontend/el7a2ny-frontend/src/layouts/auth/layout.js
@@ -1,9 +1,7 @@
-import PropTypes from 'prop-types';
-import NextLink from 'next/link';
-import { Box, Typography, Unstable_Grid2 as Grid } from '@mui/material';
-import { Logo } from 'src/components/logo';
-
-// TODO: Change subtitle text
+import PropTypes from "prop-types";
+import NextLink from "next/link";
+import { Box, Unstable_Grid2 as Grid } from "@mui/material";
+import { Logo } from "src/components/logo";
 
 export const Layout = (props) => {
   const { children } = props;
@@ -12,22 +10,19 @@ export const Layout = (props) => {
     <Box
       component="main"
       sx={{
-        display: 'flex',
-        flex: '1 1 auto'
+        display: "flex",
+        flex: "1 1 auto",
       }}
     >
-      <Grid
-        container
-        sx={{ flex: '1 1 auto' }}
-      >
+      <Grid container sx={{ flex: "1 1 auto" }}>
         <Grid
           xs={12}
           lg={6}
           sx={{
-            backgroundColor: 'background.paper',
-            display: 'flex',
-            flexDirection: 'column',
-            position: 'relative'
+            backgroundColor: "background.paper",
+            display: "flex",
+            flexDirection: "column",
+            position: "relative",
           }}
         >
           <Box
@@ -35,18 +30,18 @@ export const Layout = (props) => {
             sx={{
               left: 0,
               p: 3,
-              position: 'fixed',
+              position: "fixed",
               top: 0,
-              width: '100%'
+              width: "100%",
             }}
           >
             <Box
               component={NextLink}
               href="/"
               sx={{
-                display: 'inline-flex',
+                display: "inline-flex",
                 height: 32,
-                width: 32
+                width: 32,
               }}
             >
               <Logo />
@@ -58,47 +53,18 @@ export const Layout = (props) => {
           xs={12}
           lg={6}
           sx={{
-            alignItems: 'center',
-            background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
-            color: 'white',
-            display: 'flex',
-            justifyContent: 'center',
-            '& img': {
-              maxWidth: '100%'
-            }
+            alignItems: "center",
+            background: "radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)",
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            "& img": {
+              maxWidth: "100%",
+            },
           }}
         >
           <Box sx={{ p: 3 }}>
-            {/* <Typography
-              align="center"
-              color="inherit"
-              sx={{
-                fontSize: '24px',
-                lineHeight: '32px',
-                mb: 1
-              }}
-              variant="h1"
-            >
-              Welcome to{' '}
-              <Box
-                component="a"
-                sx={{ color: '#15B79E' }}
-                target="_blank"
-              >
-                CodeMedics-Pharmacy
-              </Box>
-            </Typography>
-            <Typography
-              align="center"
-              sx={{ mb: 1 }}
-              variant="subtitle1"
-            >
-             Your Health, Our Care.
-            </Typography> */}
-            <img
-              alt=""
-              src="/assets/Clinic.png"
-            />
+            <img alt="" src="/assets/Clinic.png" />
           </Box>
         </Grid>
       </Grid>
@@ -107,5 +73,5 @@ export const Layout = (props) => {
 };
 
 Layout.prototypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
