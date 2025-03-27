208164faf0318812b79f2a619e852f5f77667b9a
omar mohammed <omarb2515@gmail.com>
2025-03-27 23:32:08 +0200

    change code structure

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -1,65 +1,47 @@
 const express = require("express");
 const router = express.Router();
+
+// ==========================
+// Controllers
+// ==========================
+
+// Admin
 const AdminController = require("../controllers/Admin/AdminController");
-const viewDoctorApplications = require("../controllers/Admin/DoctorsApplications");
-const { getPackages } = require("../controllers/Admin/AdminController");
 const {
+  getPackages,
   changePassword,
-  acceptRejectDoctorRequest,
 } = require("../controllers/Admin/AdminController");
-const nodemailer = require("nodemailer");
-const bcrypt = require("bcryptjs");
-const crypto = require("crypto");
-const adminModel = require("../models/Administrator");
-const viewDoctors = require("../controllers/Admin/viewDoctors");
-const viewPatients = require("../controllers/Admin/viewPatients");
-const removePatient = require("../controllers/Admin/removePatient");
-const removeDoctor = require("../controllers/Admin/removeDoctor");
 const viewAdmins = require("../controllers/Admin/viewAdmins");
-const removeAdmin = require("../controllers/Admin/removeAdmin");
 const addAdmin = require("../controllers/Admin/addAdmin");
+const removeAdmin = require("../controllers/Admin/removeAdmin");
+
+// Doctors
+const viewDoctors = require("../controllers/Admin/viewDoctors");
+const removeDoctor = require("../controllers/Admin/removeDoctor");
 const {
   createAndDownloadContract,
 } = require("../controllers/Admin/acceptDoctor");
-const Doctor = require("../models/Doctor");
-//const JWTAuth = require('../config/JWTAuth.js');
 
-//const isAuth = JWTAuth.isAuth;
+// Patients
+const viewPatients = require("../controllers/Admin/viewPatients");
+const removePatient = require("../controllers/Admin/removePatient");
+const { validateDoctor } = require("../utils/validator");
 
-// Middleware to check authentication for the routes below
-//router.use(isAuth);
-//admin
+// ==========================
+// Routes
+// ==========================
 
+// Admin Routes
 router.get("/", viewAdmins);
 router.get("/packages", getPackages);
-router.get("/viewDoctorApplications", viewDoctorApplications);
-router.get("/doctors", viewDoctors);
-router.get("/doctors/applications", AdminController.getDoctorsReg);
-router.get("/patients", viewPatients);
-
 router.post("/", addAdmin);
 router.patch("/:adminUsername", changePassword);
-router.delete("patients/:patientUsername", removePatient);
-router.delete("/doctors/:doctorUsername", removeDoctor);
 router.delete("/:adminUsername", removeAdmin);
 
-router.post("/createAdmin", (req, res) => {
-  AdminController.createAdmin(req, res).then();
-});
-
-router.delete("/removeUser", (req, res) => {
-  AdminController.removeUser(req, res).then();
-});
-router.patch("/updateAdmin", (req, res) => {
-  AdminController.updateAdmin(req, res).then();
-});
-
-router.get("/getAllAdmins", (req, res) => {
-  AdminController.getAllAdmins(req, res).then();
-});
-
-//doctor
-
+// Doctor Routes
+router.get("/doctors", viewDoctors);
+router.get("/doctors/applications", AdminController.getDoctorsReg);
+router.delete("/doctors/:doctorUsername", removeDoctor);
 router.patch(
   "/doctors/:doctorUsername/accept",
   AdminController.acceptDoctorRequest
@@ -69,34 +51,28 @@ router.patch(
   AdminController.rejectDoctorRequest
 );
 
+// Patient Routes
+router.get("/patients", viewPatients);
+router.delete("/patients/:patientUsername", removePatient);
+
+// Contract Generation Route
 router.post("/download-contract", async (req, res) => {
   try {
     const doctorUsername = req.body.doctor;
-    // Fetch the doctor object based on the username (doctorUsername) from your data source
-    const doctor = await Doctor.findOne({ Username: doctorUsername });
-
-    // Create and download the contract based on the doctor object
+    const doctor = await validateDoctor(doctorUsername, res);
     const pdfBuffer = await createAndDownloadContract(doctor);
 
-    // Send the PDF buffer as a response
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Disposition", `attachment; filename="Contract.pdf"`);
     res.status(200).send(pdfBuffer);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
-//packages
-
-router.post("/packages", (req, res) => {
-  AdminController.addPackage(req, res).then();
-});
-router.delete("/packages/:packageName", (req, res) => {
-  AdminController.removePackage(req, res).then();
-});
-router.patch("/packages/:packageName", (req, res) => {
-  AdminController.updatePackage(req, res).then();
-});
+// Package Management Routes
+router.post("/packages", AdminController.addPackage);
+router.delete("/packages/:packageName", AdminController.removePackage);
+router.patch("/packages/:packageName", AdminController.updatePackage);
 
 module.exports = router;
