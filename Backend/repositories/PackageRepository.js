d85856d7d9ede184bde90c94dcc51c6b7e9b0c52
omar mohammed <omarb2515@gmail.com>
2025-04-18 19:16:23 +0200

    create package repository

---
+++ Backend/repositories/PackageRepository.js
@@ -0,0 +1,18 @@
+const Package = require("../../models/Package");
+
+exports.validatePackage = async (packageName) => {
+  const package = await this.getPackage(packageName);
+  if (!package) {
+    const error = new Error("Package not found");
+    error.statusCode = 404;
+    throw error;
+  }
+  return package;
+};
+
+exports.getPackage = async (packageName) => {
+  const package = await Package.findOne({
+    name: packageName,
+  });
+  return package;
+};
