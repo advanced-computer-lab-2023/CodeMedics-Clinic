9d87db191f63f5d13a6f0335582da8bbaf3980d9
omar mohammed <omarb2515@gmail.com>
2025-04-20 06:45:11 +0200

    remove unused parameter

--- Backend/controllers/AdminController.js
+++ Backend/controllers/AdminController.js
@@ -83,7 +83,6 @@ router.delete("/doctors/:doctorUsername", async (req, res) => {
 router.patch("/doctors/:doctorUsername/accept", async (req, res) => {
   try {
     const doctor = await adminService.acceptDoctor(
-      req.params.username,
       req.params.doctorUsername
     );
     res.status(200).json({ data: doctor });
@@ -95,7 +94,6 @@ router.patch("/doctors/:doctorUsername/accept", async (req, res) => {
 router.patch("/doctors/:doctorUsername/reject", async (req, res) => {
   try {
     const doctor = await adminService.rejectDoctor(
-      req.params.username,
       req.params.doctorUsername
     );
     res.status(200).json({ data: doctor });
