ed8f49cfb808992756b3bc6bfb0302c6f14c21fc
omar mohammed <omarb2515@gmail.com>
2025-04-24 08:39:32 +0200

    remove unused files

--- controllers/Admin/removeDoctor.js
+++
@@ -1,8 +0,0 @@
-const Doctor = require("../../models/Doctor");
-const removeDoctor = async (req, res) => {
-  const { doctorUsername } = req.params;
-  await Doctor.deleteOne({ username: doctorUsername });
-  return res.status(200).json({ message: "Doctor removed." });
-};
-
-module.exports = removeDoctor;
